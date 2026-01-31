6efe5586b1a082e4afccf7add343bd2b057ededc
SajaSaad6 <sajasaad022@gmail.com>
2026-01-31 02:51:50 +0100

    Refactor enums: move ErrorInput and Gender to enums package

--- src/test/java/com/qa/guru99bank/datatest/Gender.java
+++
@@ -1,8 +0,0 @@
-package com.qa.guru99bank.datatest;
-
-public enum Gender {
-	MALE("m"), FEMALE("f");
-    private final String value;
-    Gender(String value) { this.value = value; }
-    public String getValue() { return value; }
-}
